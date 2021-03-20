28f9836dd2d0239ada79872b44d75908bdc3657d
levermory <lev.schepelin@icloud.com>
2021-03-20 11:11:57 +0300

    init

---
+++ LB4/src/com/mmf/Main.java
@@ -0,0 +1,11 @@
+//A 14. Создать объект класса Простая дробь, используя класс Число. Методы:
+//вывод на экран, сложение, вычитание, умножение, деление.
+
+package com.mmf;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
