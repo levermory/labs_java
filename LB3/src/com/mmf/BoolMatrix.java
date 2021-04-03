cc1d81787e0d7ba68ef283e586e137c40a5480bb
levermory <lev.schepelin@icloud.com>
2021-04-03 09:45:16 +0300

    completed

--- LB3/src/com/mmf/BoolMatrix.java
+++ LB3/src/com/mmf/BoolMatrix.java
@@ -1,7 +1,97 @@
 package com.mmf;
 
+ import java.util.Arrays;
+
 public class BoolMatrix {
-    void test(){
-        System.out.print("hello world");
+    private final int n, m;
+    private Boolean[][] data;
+
+    public BoolMatrix(){
+        n = 3;
+        m = 3;
+        data = new Boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                data[i][j] = false;
+                if(Math.random()>=0.5){
+                    data[i][j] = true;
+                };
+            }
+        }
+    }
+    public BoolMatrix(int n, int m){
+        this.n = n;
+        this.m = m;
+        data = new Boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                data[i][j] = false;
+                if(Math.random()>=0.5){
+                    data[i][j] = true;
+                };
+            }
+        }
+    }
+    public BoolMatrix(Integer[][] data){
+        this.n = data.length;
+        this.m = data[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(data[i][j]>0) {
+                    this.data[i][j] = true;
+                }
+                else {
+                    this.data[i][j] = false;
+                }
+            }
+        }
+    }
+    public void add(BoolMatrix M){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                this.data[i][j] = (this.data[i][j] || M.data[i][j]);
+            }
+        }
+    }
+    public void mult(BoolMatrix M){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                this.data[i][j] = (this.data[i][j] && M.data[i][j]);
+            }
+        }
+    }
+    public int count(){
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(this.data[i][j]) res++;
+            }
+        }
+        return res;
+    }
+    public void inverse(){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                this.data[i][j] = !(this.data[i][j]);
+            }
+        }
+    }
+     public void sort(){
+         Arrays.sort(data, new cmp());
+     }
+    public String toString(){
+        String ans = "";
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(data[i][j]){
+                    ans += "1 ";
+                }
+                else {
+                    ans += "0 ";
+                }
+            }
+            ans += "\n";
+        }
+        return ans;
     }
 }
--- LB3/src/com/mmf/Main.java
+++ LB3/src/com/mmf/Main.java
@@ -6,10 +6,41 @@
 
 package com.mmf;
 
+ import java.util.Comparator;
+
 public class Main {
 
     public static void main(String[] args) {
-        var test = new BoolMatrix();
-        test.test();
+       
+        BoolMatrix M1 = new BoolMatrix(5,6);
+        BoolMatrix M2 = new BoolMatrix(5,6);
+        System.out.println(M1);
+        System.out.println(M2);
+        M1.add(M2);
+        System.out.println(M1);
+        M1.mult(M2);
+        System.out.println(M1);
+        System.out.println(M1.count());
+        M1.inverse();
+        System.out.println(M1);
+        M1.sort();
+        System.out.println(M1);
     }
 }
+
+ class cmp implements Comparator<Boolean[]> {
+     public int compare(Boolean[] a, Boolean[] b) {
+         int res=0;
+         for (int i = 0; i < a.length; i++) {
+             if(a[i] && !b[i]){
+                 res= -1; break;
+             }
+             else if(!a[i] && b[i]){
+                 res= 1; break;
+             }
+             else res = 0;
+         }
+         return res;
+        
+     }
+ }
